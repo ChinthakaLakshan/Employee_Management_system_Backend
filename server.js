b6f2507f90e2561f601392563d75a9013bc6b202
ChinthakaLakshan <88089519+ChinthakaLakshan@users.noreply.github.com>
2023-06-08 16:37:38 +0530

    2 nd

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/', require('./routes/root'))
 app.use('/users', require('./routes/userRoutes'))
 app.use('/employee', require('./routes/employeeRoutes'))
+app.use('/auth', require('./routes/authRoutes'))
 
 
 app.all('*', (req, res) => {
