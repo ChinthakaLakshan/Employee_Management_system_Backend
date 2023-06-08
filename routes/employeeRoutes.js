c8a13005b78cf3465a65201049762cb74d78daa7
ChinthakaLakshan <88089519+ChinthakaLakshan@users.noreply.github.com>
2023-06-09 00:43:48 +0530

    fourth commit

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 const employeeController=require('../controllers/employeeController')
 
+const verifyJWT = require('../middleware/verifyJWT')
+
+router.use(verifyJWT)
+
 router.route('/')
 .get(employeeController.getAllEmployees)
 .post(employeeController.createNewEmployees)
